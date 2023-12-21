ce46a6f66c1fc7864233c00a899d4dcb4326e995
TRexx <bismit77@gmail.com>
2023-12-21 18:40:20 +0530

    Book Appointment landing design complete

--- registration_portal/src/Components/BookAppointment/BookAppointmentLanding.js
+++ registration_portal/src/Components/BookAppointment/BookAppointmentLanding.js
@@ -1,13 +1,71 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import PatientCard from '../Navbar/PatientCard'
+import './BookAppointmentLanding.css'
+import opdCheckin from "../../Assests/Images/opdCheckin.svg"
+import bookConsultant from "../../Assests/Images/bookConsultant.svg"
+import billing from "../../Assests/Images/billing.svg"
+import labReport from "../../Assests/Images/labReport1.svg"
 
 function BookAppointmentLanding() {
   return (
     <>
+    <div style={{background:'#EFF2F7', height:'100vh'}}>    
      <Navbar pagename={'Already Registered'}/>
      <PatientCard/>
 
+     <div className='landingContentBox'>
+      <div className='blockContent'>
+         <div>
+          <img src={opdCheckin} alt="OPD CheckIn" />
+         </div>
+         <div className='imgTitle'>
+         OPD Check-in
+         </div>
+         <div className='imgSubTitle'>
+         for pre-booked appointments
+         </div>
+      </div>
+
+      <div className='blockContent'>
+      <div>
+          <img src={bookConsultant} alt="bookConsultant" />
+         </div>
+         <div className='imgTitle'>
+         Book Consultation
+         </div>
+         <div className='imgSubTitle'>
+         Book new appointment with doctor
+         </div>
+      </div>
+
+      <div className='blockContent'>
+      <div>
+          <img src={labReport} alt="labReport" />
+         </div>
+         <div className='imgTitle'>
+         Lab Reports
+         </div>
+         <div className='imgSubTitle'>
+         Get your lab test report
+         </div>
+      </div>
+
+      <div className='blockContent'>
+      <div>
+          <img src={billing} alt="billing" />
+         </div>
+         <div className='imgTitle'>
+         Billing
+         </div>
+         <div className='imgSubTitle'>
+         Pay your Bills & generate invoice
+         </div>
+      </div>
+
+     </div>
+
+     </div>
 
     </>
   )
